d8d47aff975dc9e01379b9f0887ff8a611957d69
PawelSzczepaniak99 <61648162+PawelSzczepaniak99@users.noreply.github.com>
2020-10-18 13:29:42 +0200

    Add files via upload

---
+++ 1.Początki/1.początki.js
@@ -0,0 +1,22 @@
+// Zadanie 1:
+
+console.log("Paweł");
+
+
+//Zadanie 2:
+
+console.log(12+32);
+console.log(52/16);
+console.log(5*2);
+
+
+//Zadanie 3:
+
+
+console.log("Hello World");
+
+console.log(Hello World); //Przy braku nawiasów kompilotr oczekuje zmiennej.
+
+console.log("Hello World); // Błąd ponieważ String nie został zakończony.
+
+
