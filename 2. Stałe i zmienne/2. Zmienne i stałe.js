462bb19899108d858d9ef9827851c9ac233b878c
PawelSzczepaniak99 <61648162+PawelSzczepaniak99@users.noreply.github.com>
2020-10-21 07:59:02 +0200

    Add files via upload

---
+++ 2. Stałe i zmienne/2. Zmienne i stałe.js
@@ -0,0 +1,31 @@
+//Zadanie 1 
+
+const num = 1;
+str = 'str';
+combo = num + str; 
+boll = true;
+nothing = null;
+
+console.log(num);
+console.log(str);
+console.log(combo);
+console.log(boll);
+console.log(nothing);
+
+
+//Zadanie 2 
+
+let num1 = 1;
+num2 = 2;
+sum = 0;
+
+sum = (num1 + num2);
+
+console.log(sum);
+
+
+//Zadanie 3
+
+str2 = ;
+
+console.log(str2); //Zmienna nie posiada zadeklarowanej warości.
